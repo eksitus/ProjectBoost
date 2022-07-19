489cf006b23dac09f230dc718ebb61dafbce0c6e
eksitus <uvay02@gmail.com>
2022-07-20 00:19:44 +0300

    .

--- ProjectBoostt/Assets/Scripts/Movement.cs
+++ ProjectBoostt/Assets/Scripts/Movement.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    Rigidbody rigidBody;
+
+    [SerializeField]float thrust = 1000f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+
+
+    void ProcessThrust()
+    {   
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(0,thrust*Time.deltaTime,0);
+        }
         
+    }
+
+
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.01f, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.01f, 0, 0);
+        }
     }
 }
